bde5efc4366be74cad58f165e7a7adab59a247b4
Nabi Bux <55255396+NabiBux9565@users.noreply.github.com>
2020-07-15 12:53:35 +0500

    Add files via upload

---
+++ ENUM.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+    'declaring ENUM 
+
+    Enum season
+        summer = 1
+        winter = 2
+        autumn = 5
+        spring = 6
+
+    End Enum
+
+    Sub Main()
+        'using enum
+
+        Dim weather As season
+        weather = season.summer
+        Console.WriteLine("weather " & weather)
+        Console.ReadKey()
+    End Sub
+
+End Module
